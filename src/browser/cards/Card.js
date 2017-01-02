d33afcafb896cecfb0648445a31b9a4f6f1fe6cf
KJ <xiankai@gmail.com>
2017-01-02 22:03:33 +0800

    Fix for A side

--- src/browser/cards/Card.js
+++ src/browser/cards/Card.js
@@ -18,10 +18,10 @@ const Card = ({ card, isPlayer }) => {
         // flex: 1,
       }}
     >
-      <div style={{ position: 'absolute', top: '5%', left: '45%' }}>{topValue}</div>
-      <div style={{ position: 'absolute', left: '5%', top: '45%' }}>{leftValue}</div>
-      <div style={{ position: 'absolute', bottom: '5%', left: '45%' }}>{bottomValue}</div>
-      <div style={{ position: 'absolute', right: '5%', top: '45%' }}>{rightValue}</div>
+      <div style={{ position: 'absolute', top: '5%', left: '45%' }}>{topValue === "10" ? 'A' : topValue}</div>
+      <div style={{ position: 'absolute', left: '5%', top: '45%' }}>{leftValue === "10" ? 'A' : leftValue}</div>
+      <div style={{ position: 'absolute', bottom: '5%', left: '45%' }}>{bottomValue === "10" ? 'A' : bottomValue}</div>
+      <div style={{ position: 'absolute', right: '5%', top: '45%' }}>{rightValue === "10" ? 'A' : rightValue}</div>
     </div>
   );
 };
