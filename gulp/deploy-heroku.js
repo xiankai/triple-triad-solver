4274e2b9d04984fa4468de3849a1e9a2adc950aa
KJ <xiankai@gmail.com>
2017-01-14 10:56:58 +0800

    Heroku deploy

--- gulp/deploy-heroku.js
+++ gulp/deploy-heroku.js
@@ -2,5 +2,5 @@ import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
 gulp.task('deploy-heroku', (done) => {
-  runSequence('deploy-heroku-git', 'deploy-firebase-database', done);
+  runSequence('deploy-heroku-git', done);
 });
